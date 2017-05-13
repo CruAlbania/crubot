308247384a7442e1423f93d56bb6031e495b09c5
Gordon Burgett <gordon@gordonburgett.net>
2017-05-13 13:37:32 +0200

    add gulp clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp')
 var ts = require('gulp-typescript')
 var rename = require('gulp-rename')
+var del = require('del')
+var vinylPaths = require('vinyl-paths')
 
 const tsproj = ts.createProject('tsconfig.json')
 
@@ -16,3 +18,12 @@ gulp.task('build', () => {
 gulp.task('watch', ['build'], () => 
   gulp.watch(['src/**/*.ts', '!src/**/*.d.ts', '!src/**/*.test.ts'], ['build'])
 )
+
+gulp.task('clean', function () {
+  return gulp.src(['src/**/*.ts', '!src/**/*.d.ts', '!src/**/*.test.ts'], { read: false })
+    .pipe(rename({
+      extname: ".js"
+    }))
+    .pipe(gulp.dest('scripts'))
+    .pipe(vinylPaths(del))
+})
